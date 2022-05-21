e8fcae8749f57c9a1c05fd4d103e2396427284ef
[Rajat Jakhar] <[rajatjakhar03@gmail.com]>
2022-05-21 08:56:52 +0530

    footer changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 // import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
-import { Link} from 'react-router-dom';
+
 
  export function Navbar(props){
     return (
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 export function Footer(){
     return (
-        <div className=' fixed-bottom footerchanged'>
-          <p>Copyright &copy; 2022-- Uppercase.com All Rights reserved </p>
+        <div className=' container footerchanged'>
+          <p>Copyright &copy; 2022-- Uppercase.com </p>
         </div>
 
     );
